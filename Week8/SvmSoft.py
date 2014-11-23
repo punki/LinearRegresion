e6f58bcb57f53d9368eb195159d631df99b38bff
punki <tomasz.pankowski@gmail.com>
2014-11-23 23:56:48 +0100

    week8

--- Week8/SvmSoft.py
+++ Week8/SvmSoft.py
@@ -3,12 +3,43 @@ from common.DataSet import DataSet
 import time, random, numpy as np
 from sklearn import svm
 
+
+def compute_error(clf, x, target):
+    predict = clf.predict(x)
+    return len(target[target != predict]) / float(len(target))
+
+
 training_data_set = DataSet('features.train.txt')
 test_data_set = DataSet('features.test.txt')
+classes = np.unique(training_data_set.get_y())
 
-clf = svm.SVC(kernel='poly', C=0.01, degree=2)
-clf.fit(training_data_set.get_x(), training_data_set.get_target())
+e_in_for_n = []
+e_out_for_n = []
 
-test_predict = clf.predict(test_data_set.get_x())
-test_target = test_data_set.get_target()
-print 'Error: {}'.format(len(test_target[test_target != test_predict])/float(len(test_target)))
+for n in range(1, len(classes)):
+    # classes_split = np.array_split(classes, len(classes)-n)
+    e_in_all = []
+    e_out_all = []
+    for idx in range(0, len(classes) - (n - 1)):
+        for C in { 0.001, 0.01, 0.1, 1}:
+            cs = classes[idx:idx + n]
+            clf = svm.SVC(kernel='poly', C=C, degree=2)
+            training_n_versus = training_data_set.new_n_versus_all(cs)
+            test_n_versus = test_data_set.new_n_versus_all(cs)
+            clf.fit(training_n_versus.get_x(), training_n_versus.get_y())
+
+            print 'C={} n={} svm={}'.format(C, n, clf.n_support_)
+
+            e_in = compute_error(clf, training_n_versus.get_x(), training_n_versus.get_y())
+            e_out = compute_error(clf, test_n_versus.get_x(), test_n_versus.get_y())
+            e_in_all.append(e_in)
+            e_out_all.append(e_out)
+
+    e_in_mean = np.mean(e_in_all)
+    e_out_mean = np.mean(e_out_all)
+    e_in_for_n.append((n, e_in_mean))
+    e_out_for_n.append((n, e_out_mean))
+    print 'n={} Ein: {}'.format(n, e_in_mean)
+    print 'n={} Eout: {}'.format(n, e_out_mean)
+
+print 'Min ein: {}'.format(min(e_in_for_n, key=lambda i: i[1]))
--- common/DataSet.py
+++ common/DataSet.py
@@ -3,13 +3,30 @@ import numpy as np
 
 
 class DataSet:
-    def __init__(self, fileName):
-        self.targetColumn = 0
+    def __init__(self, fileName = None, data=None):
+        self.classColumn = 0
         self.numberOfFeatures = 2
-        self.data = np.fromfile(fileName, float, -1, ' ').reshape(-1, self.numberOfFeatures + 1)
+        if fileName is not None:
+            self.data = np.fromfile(fileName, float, -1, ' ').reshape(-1, self.numberOfFeatures + 1)
+        else:
+            self.data = data
+        self.x = self.only_x(self.data)
+        self.y = self.only_y(self.data)
+
+    def only_x(self, data):
+        return np.delete(data, self.classColumn, self.classColumn + 1)
+
+    def only_y(self,data):
+        return data[:, self.classColumn]
 
     def get_x(self):
-        return np.delete(self.data, self.targetColumn, self.targetColumn + 1)
+        return self.x
 
-    def get_target(self):
-        return self.data[:, self.targetColumn]
+    def get_y(self):
+        return self.y
+
+    def new_n_versus_all(self, n_classes):
+        new_data = np.copy(self.data)
+        for d in new_data:
+            d[self.classColumn] = -1 if d[self.classColumn] not in n_classes else 1
+        return DataSet(data=new_data)
